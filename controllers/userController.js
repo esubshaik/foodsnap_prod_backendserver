52c86d0efe57dd5a819f962220a8d6a7e853b188
Esub Shaik <esubshaik7864@gmail.com>
2023-10-13 00:16:16 +0530

    fixed otp issue #1

--- controllers/userController.js
+++ controllers/userController.js
@@ -193,9 +193,7 @@ const sendotp = async (req, res) => {
     const { email } = req.body;
     console.log(email)
     if (!email) return res.status(400).json({ message: "Fill Email Properly" });
-    const isUser = await User.findOne({ email });
-
-    if (!isUser) {
+    
       const otp = randomstring.generate({
         length: 6, // Adjust the length as needed
         charset: 'numeric'
@@ -230,7 +228,6 @@ const sendotp = async (req, res) => {
         }
       });
       return res.status(200).json({ message: "OTP Sent Successfully!",OTP: `${otp}` });
-    }
     
   }
   catch (err) {
