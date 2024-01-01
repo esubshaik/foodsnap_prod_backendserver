fd5c9575e90db6826340bae49779830d5491717e
Esub Shaik <esubshaik7864@gmail.com>
2024-01-01 18:13:33 +0530

    all entries

--- controllers/userController.js
+++ controllers/userController.js
@@ -307,7 +307,7 @@ const getNutriData = async (req, res) => {
     });
 
     console.log(entriesToday);
-    return res.status(200).json({ entries: entriesToday,allentries });
+    return res.status(200).json({ entries: entriesToday,allentries: allentries });
   } catch (error) {
     console.error("Error fetching entries:", error);
     return res.status(500).json({ message: error.message });
