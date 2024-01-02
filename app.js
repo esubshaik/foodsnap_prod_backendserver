f156fa49552a00b0ddb7ea43ee48ee0d02feda0e
Esub Shaik <esubshaik7864@gmail.com>
2024-01-02 18:49:41 +0530

    added notification feature

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(cors());
 const userRouter = require("./routes/userRoute");
 const { isAuthenticated } = require("./Middleware/verifyJWT");
 const bodyParser = require('body-parser');
+const User = require("./models/User");
 
 const mongoURI = process.env.MONGODB_URL;
 mongoose
@@ -16,53 +17,72 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    // console.log("Connected to MongoDB Atlas!");
+    console.log("Connected to MongoDB Atlas!");
   })
   .catch((error) => {
-    // console.error("Error connecting to MongoDB Atlas:", error);
+    console.error("Error connecting to MongoDB Atlas:", error);
   });
 
 
-//   const { Expo } = require('expo-server-sdk');
-// const cron = require('node-cron');
-
-// // Create a new Expo client
-// const expo = new Expo();
-
-// const notifyarr = [
-//   {
-//     title: '⏰ 𝗧𝗶𝗺𝗲 𝗳𝗼𝗿 𝗮 𝗙𝗼𝗼𝗱𝗦𝗻𝗮𝗽 𝗨𝗽𝗱𝗮𝘁𝗲! ⏰',
-//     body: "🍎 Don't forget to log your latest meal. Your journey to a healthier lifestyle starts with every entry. 🌿",
-//   },
-//   {
-//     title: '⚡ 𝗤𝘂𝗶𝗰𝗸 𝗨𝗽𝗱𝗮𝘁𝗲: 𝗠𝗲𝗮𝗹 𝗧𝗶𝗺𝗲! ⚡',
-//     body: "🥗 Don't miss out on recording your latest meal. Each entry counts towards a healthier lifestyle journey. 🥦 ",
-//   },
-//   {
-//     title: '🍇 𝗙𝗼𝗼𝗱𝗦𝗻𝗮𝗽 𝗔𝗹𝗲𝗿𝘁: 𝗧𝗶𝗺𝗲 𝘁𝗼 𝗟𝗼𝗴! 🍇 ',
-//     body: "🥑 Your meal log awaits! Make every entry count on your path to a healthier and happier you. 🏋️‍♀️",
-//   },
-// ];
-
-// function sendExpoNotification(expoPushToken, message) {
-//   const notification = {
-//     to: expoPushToken,
-//     sound: 'default',
-//     title: notifyarr[0].title,
-//     body: notifyarr[0].body,
-//   };
-
-//   expo.sendPushNotificationsAsync([notification])
-//     .then((receipts) => {
-//       console.log('Notification sent successfully:', receipts);
-//     })
-//     .catch((error) => {
-//       console.error('Error sending notification:', error);
-//     });
-// }
-
-// const notificationMessage = 'Your Notification Message';
-
+  const { Expo } = require('expo-server-sdk');
+  const cron = require('node-cron');
+  
+  // Create a new Expo client
+  const expo = new Expo();
+  
+  const notifyarr = [
+    {
+      title: '⏰ 𝗧𝗶𝗺𝗲 𝗳𝗼𝗿 𝗮 𝗙𝗼𝗼𝗱𝗦𝗻𝗮𝗽 𝗨𝗽𝗱𝗮𝘁𝗲! ⏰',
+      body: "🍎 Don't forget to log your latest meal. Your journey to a healthier lifestyle starts with every entry. 🌿",
+    },
+    {
+      title: '⚡ 𝗤𝘂𝗶𝗰𝗸 𝗨𝗽𝗱𝗮𝘁𝗲: 𝗠𝗲𝗮𝗹 𝗧𝗶𝗺𝗲! ⚡',
+      body: "🥗 Don't miss out on recording your latest meal. Each entry counts towards a healthier lifestyle journey. 🥦 ",
+    },
+    {
+      title: '⌚ 𝗙𝗼𝗼𝗱𝗦𝗻𝗮𝗽 𝗔𝗹𝗲𝗿𝘁: 𝗧𝗶𝗺𝗲 𝘁𝗼 𝗟𝗼𝗴! ⌚',
+      body: "🍔 Your meal log awaits! Make every entry count on your path to a healthier and happier you. 🌾",
+    },
+  ];
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+  
+  function sendExpoNotification(expoPushToken, title, body) {
+    const notification = {
+      to: expoPushToken,
+      sound: 'default',
+      title,
+      body,
+    };
+  
+    expo.sendPushNotificationsAsync([notification])
+      .then((receipts) => {
+        console.log('Notification sent successfully:', receipts);
+      })
+      .catch((error) => {
+        console.error('Error sending notification:', error);
+      });
+  }
+  
+  let notificationMessage;
+  if (currentHour === 7 || currentHour === 13 || currentHour === 18) {
+    const index = currentHour === 7 ? 0 : currentHour === 13 ? 1 : 2;
+    notificationMessage = notifyarr[index];
+  
+    cron.schedule(`47 ${currentHour} * * *`, async() => {
+      const allusers = await User.find({}) ;
+      for(var i = 0 ; i < allusers.length; i++){
+        if(allusers[i].pushtoken != ''){
+          sendExpoNotification(allusers[i].pushtoken, notificationMessage.title, notificationMessage.body);
+        }
+      }
+      // const expoPushToken = 'ExponentPushToken[8LSRkkItMdZQGn_lGWdF1V]';
+      // sendExpoNotification(expoPushToken, notificationMessage.title, notificationMessage.body);
+    }, {
+      timezone: 'Asia/Kolkata',
+    });
+  }
+  
 // cron.schedule('* * * * *', () => {
 //   const expoPushToken = 'ExponentPushToken[8LSRkkItMdZQGn_lGWdF1V]';
 //   sendExpoNotification(expoPushToken, notificationMessage);
